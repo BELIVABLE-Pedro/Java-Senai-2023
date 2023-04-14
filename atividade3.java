5e8bc4585ef145a1c81a147356de602059b84c8e
Pedro Joaquim Moraes Rocha <125685178+BELIVABLE-Pedro@users.noreply.github.com>
2023-04-14 14:15:10 -0300

    atividade 3

---
+++ atividade3.java
@@ -0,0 +1,16 @@
+// Cáuculo da área do Triângulo
+
+public class atividade3{
+
+public static final double BASE = 5.5; // A base é uma constante
+
+public static void main(string[] args) {
+
+    double altura = 4.0;
+    double area = (BASE*altura)/2;
+    System.out.println("A área do Triângulo é:"+area+"cm²");
+}
+
+
+
+}
